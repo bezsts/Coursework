0f4614ab28aa6fd56e3f696162b6942cb0411495
bezst <bezst29@gmail.com>
2024-12-23 21:20:31 +0200

    Add methods to handle request parametres in scenario manager

--- WPF/Models/ScenarioManager.cs
+++ WPF/Models/ScenarioManager.cs
@@ -1,6 +1,7 @@
 ﻿using NBomber.Contracts;
 using NBomber.CSharp;
 using WPF.Common;
+using WPF.Models.Requests;
 using WPF.Models.Scenarious;
 
 namespace WPF.Models
@@ -8,21 +9,39 @@ namespace WPF.Models
     class ScenarioManager
     {
         private readonly List<BaseScenario> _scenarious;
+        private readonly List<RequestParametres> _requestParametres;
 
         public ScenarioManager()
         {
             _scenarious = new List<BaseScenario>();
+            _requestParametres = new List<RequestParametres>();
         }
 
         public IEnumerable<BaseScenario> GetScenarios()
-        { 
+        {
             return _scenarious;
+        }
+
+        public IEnumerable<RequestParametres> GetRequestParametres()
+        {
+            return _requestParametres;
         }
 
         public BaseScenario AddScenario(BaseScenario scenario)
         {
             _scenarious.Add(scenario);
             return scenario;
+        }
+
+        public RequestParametres AddRequestParametres(RequestParametres requestParametres)
+        { 
+            _requestParametres.Add(requestParametres);
+            return requestParametres;
+        }
+
+        public void AddRequestParametresToScenario(RequestParametres requestParametres, BaseScenario scenario)
+        { 
+            scenario.RequestParametres = requestParametres;
         }
 
         public void RunScenarious(IEnumerable<BaseScenario> scenarious)
